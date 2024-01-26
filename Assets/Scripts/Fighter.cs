dfd81a41295d41f058e8b36045ee8fb5a1b7b7ca
okkero <oksandum@gmail.com>
2024-01-26 22:25:52 +0100

    Add Fighter script stub

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FighterCharacterType
+{
+    NotSonic,
+    NotPikachu,
+    NotMario,
+}
+
+public class Fighter : MonoBehaviour
+{
+    public FighterCharacterType character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
