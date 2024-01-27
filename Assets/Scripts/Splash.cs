c44c2a6edc48d4f10b2f070d7725f428519524f4
okkero <oksandum@gmail.com>
2024-01-27 23:13:32 +0100

    Add splash screen

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.anyKeyDown) SceneManager.LoadScene("Scenes/Game");
+    }
+}
