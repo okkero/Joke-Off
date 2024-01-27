a1275cfdd85294ebc12cfa57a0cbaeca6ff75c4c
okkero <oksandum@gmail.com>
2024-01-27 13:04:10 +0100

    Input handling stubs for Player 1

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -1,26 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public enum FighterCharacterType
 {
     NotSonic,
     NotPikachu,
-    NotMario,
+    NotMario
+}
+
+public enum Attack
+{
+    Hi,
+    Ha,
+    Ho,
+    He
 }
 
 public class Fighter : MonoBehaviour
 {
     public FighterCharacterType character;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        var gameController = FindObjectOfType<GameController>();
+        Debug.unityLogger.Log($"State is {gameController.State}");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    private void OnAttack(Attack attack)
+    {
+        Debug.unityLogger.Log($"Attack {attack}");
+    }
+
+    private void OnHiAttack()
+    {
+        OnAttack(Attack.Hi);
+    }
+
+    private void OnHaAttack()
+    {
+        OnAttack(Attack.Ha);
+    }
+
+    private void OnHoAttack()
+    {
+        OnAttack(Attack.Ho);
+    }
+
+    private void OnHeAttack()
+    {
+        OnAttack(Attack.He);
     }
 }
