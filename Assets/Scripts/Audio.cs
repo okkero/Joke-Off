d43c3af06b1788902aeca04a26ba8b7377cc4b8b
Santset <Kristofferh.privat@gmail.com>
2024-01-28 14:05:28 +0100

    Fjerne lokale filer som skapte mergefeil

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio
+{
+    public AudioClip clip;
+    public float volume;
+    public float pitch;
+    
+}
