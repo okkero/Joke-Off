029674ddaeb21cdf33abbda88e73be0b1322378e
Yu He <yuhe00@gmail.com>
2024-01-28 13:26:02 +0100

    Health bar thingy

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,6 +6,7 @@ public class HealthBar : MonoBehaviour
     private const int Threshold = 10;
 
     public int value;
+    private static readonly int Value = Shader.PropertyToID("_value");
 
     // Start is called before the first frame update
     private void Start()
@@ -15,6 +16,8 @@ public class HealthBar : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        var normalizedValue = Unity.Mathematics.math.remap(-10.0f, 10.0f, 0.0f, 1.0f, (float)value);
+        GetComponent<SpriteRenderer>().material.SetFloat(Value, normalizedValue);
     }
 
     /// <summary>
@@ -36,9 +39,9 @@ public class HealthBar : MonoBehaviour
                 throw new ArgumentOutOfRangeException(nameof(playerIndex), playerIndex, null);
         }
 
-        if (value > Threshold)
+        if (value >= Threshold)
             return PlayerIndex.Player1;
-        if (value < -Threshold)
+        if (value <= -Threshold)
             return PlayerIndex.Player2;
         return null;
     }
